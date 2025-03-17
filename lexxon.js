86ab37b349d3d1ceed079bf37d63d64c348987d9
memphisbarbecue <dkoranek4311@gmail.com>
2025-03-17 09:03:04 -0400

    V. 0.2.5 Incorporate daily puzzles vs. practice, add icons

--- lexxon.js
+++ lexxon.js
@@ -14,8 +14,12 @@ This work is composed of four parts, each of which are licensed differently.
     - memphisbarbecue (https://github.com/memphisbarbecue/)
     
 */
-slideIndex = 1
-var welcome_text = "<h1>Welcome to Lexxon!</h1>\n"
+
+version = 0.1
+mode  = "DAILY" /* Practice */
+let slideIndex = 1
+this.explanation_slideIndex = 1
+practice_puzzles_loaded = 100 
 var info_text = "<h2>Rules</h2>" +
   "<img style = 'width:80%; max-width:750px;' src='images/puzzle_diagram.png'/><br/><br/>" +
   "You are trying to identify, from the English, which of the Greek words you have available is in each verse. " +
@@ -26,16 +30,151 @@ var info_text = "<h2>Rules</h2>" +
   "</ol><br/> If you have trouble, try looking at each verse on <a href = 'https://biblehub.com/lexicon/genesis/1-1.htm'>Bible Hub's Lexicon view<a/><br/><br/>" +
   "All text and Strongs references courtesy of the <a href= 'https://berean.bible/downloads.htm'>Berean Standard Bible</a>.<br/>" +
   "Strongs lexicon definitions loaded from <a href='https://github.com/openscriptures/strongs'>the OpenScriptures GitHub repository.</a><br/>" +
-  "Logo font from <a href = 'https://actselect.chips.jp/fonts/54.htm'>this font site.</a>"
+  "Logo font from <a href = 'https://actselect.chips.jp/fonts/54.htm'>this font site.</a><br/><br/>" +
+  "<a href = 'https://www.facebook.com/profile.php?id=61573621951149'><img class='reslogo' src='images/facebook.png'/></a>"
 
 function pageLoad() {
-  this.user_cookies = document.cookie
+  //Check if valid. If not, then create a new game state
+  let page_data = null
+  if(localStorage.getItem("page_data") == null){
+    localStorage.setItem("page_data", JSON.stringify(create_page_data()))
+    this.page_data = JSON.parse(localStorage.getItem("page_data"))
+  }
+  try{
+    this.page_data = JSON.parse(localStorage.getItem("page_data"))
+  }
+  catch(e){
+
+  }
+  load_puzzle()
+  load_game_state()
   welcome()
+
+}
+function load_puzzle(){
   this.time = new Date()
-  temp_puzzle = Promise.resolve(getPuzzle(this.time.getMinutes()))
+  let new_time = new Date()
+  new_time.setTime(this.time.getTime() - (this.time.getTimezoneOffset() * 1000 * 60))
+  this.date = new_time.toISOString().substring(0,10)
+  new_time.setTime(this.time.getTime() - (this.time.getTimezoneOffset() * 1000 * 60)-1000*60*60*24)
+  this.yesterday = new_time.toISOString().substring(0,10)
+
+  temp_puzzle = Promise.resolve(getPuzzle())
   temp_puzzle.then(function (result) { setPuzzle(result) })
 }
+function load_game_state(){
 
+}
+function recordPuzzleSolvedState(){
+  if(this.page_data["play_mode"] == "DAILY"){
+    this.page_data["daily_puzzle_state"]["words_solved"][0] = this.game_state["solved"][0]
+    this.page_data["daily_puzzle_state"]["words_solved"][1] = this.game_state["solved"][1]
+    this.page_data["daily_puzzle_state"]["words_solved"][2] = this.game_state["solved"][2]
+    this.page_data["daily_puzzle_state"]["words_solved"][3] = this.game_state["solved"][3]
+    this.page_data["daily_puzzle_state"]["words_solved"][4] = this.game_state["solved"][4]
+  }
+  else if(this.page_data["play_mode"] == "PRACTICE")
+  update_local_storage()
+}
+function recordGameWin(guess_count){
+  let stats_source = ""
+  if(this.page_data["play_mode"] == "DAILY"){
+    stats_source = this.page_data["daily_puzzle_stats"]
+    if(this.date in this.page_data["history"]){
+        return
+    }
+    this.page_data["history"][this.date] = this.puzzle["wotd"]
+    if(stats_source["last_solved"] == this.yesterday){
+      stats_source["streak"] = stats_source["streak"] + 1 
+    }
+    else { 
+      stats_source["streak"] = 1
+    }
+    stats_source["last_solved"] = this.date
+    
+  }else{
+    this.page_data["practice_puzzle_state"]["current_puzzle_number"] = (this.page_data["practice_puzzle_state"]["current_puzzle_number"] + 1) % practice_puzzles_loaded
+    stats_source = this.page_data["practice_puzzle_stats"]
+  }
+  if(guess_count == 4){
+    stats_source["guesses_4"] = stats_source["guesses_4"] + 1
+  }
+  else if(guess_count == 3){
+    stats_source["guesses_3"] = stats_source["guesses_3"] + 1
+  }
+  else if(guess_count == 2){
+    stats_source["guesses_2"] = stats_source["guesses_2"] + 1
+  }
+  else if(guess_count == 1){
+    stats_source["guesses_1"] = stats_source["guesses_1"] + 1
+  }
+  update_local_storage()
+}
+function recordGameLoss(){
+  if(this.page_data["play_mode"] == "DAILY"){
+    stats_source = this.page_data["daily_puzzle_stats"]
+  }
+  else{
+    this.page_data["practice_puzzle_state"]["current_puzzle_number"] = (this.page_data["practice_puzzle_state"][current_puzzle_number] + 1) % practice_puzzles_loaded
+    stats_source = this.page_data["practice_puzzle_stats"]
+  }
+  stats_source["guesses_0"] = stats_source["guesses_0"] + 1
+}
+function update_local_storage(){
+  localStorage.setItem("page_data", JSON.stringify(this.page_data))
+}
+function create_page_data(){
+  data_dict = {
+    "version":version,
+    "daily_puzzle_stats":{
+      "guesses_4":0,
+      "guesses_3":0,
+      "guesses_2":0,
+      "guesses_1":0,
+      "guesses_0":0,
+      "last_solved":"Never",
+      "streak":0
+    },
+    "practice_puzzle_stats":{
+      "guesses_4":0,
+      "guesses_3":0,
+      "guesses_2":0,
+      "guesses_1":0,
+      "guesses_0":0,
+    },
+    "history":{
+
+    },
+    "daily_puzzle_state":{
+      "current_puzzle_date": this.date,
+      "guesses_left":4,
+      "guess_ledger":[],
+      "words_solved":{
+        0:false,
+        1:false,
+        2:false,
+        3:false,
+        4:false,
+      }
+    },
+    "play_mode": "DAILY",
+    "practice_puzzle_state":{
+      "current_puzzle_number": 0,
+      "guesses_left":4,
+      "guess_ledger":[],
+      "words_solved":{
+        0:false,
+        1:false,
+        2:false,
+        3:false,
+        4:false,
+      }
+    },
+
+    
+  }
+  return data_dict
+}
 function number_to_light_color(num) {
   switch (num) {
     case 0: return "#00a9f0";
@@ -118,8 +257,14 @@ function setPuzzle(puzzle) {
   showSlides(1)
 }
 function welcome() {
-  var innerHTML = welcome_text + info_text
-
+  let innerHTML = ""
+  if(this.page_data["play_mode"] == "DAILY"){
+    innerHTML = "<h1>Lexxon Daily Puzzle for " + this.date + "</h1>"
+  }
+  else if (this.page_data["play_mode"] == "PRACTICE"){
+    innerHTML = "<h1>Lexxon Practice Mode</h1>"
+  }
+  innerHTML = innerHTML + info_text
   overlay = generateOverlayFrameText(innerHTML)
 
 }
@@ -141,15 +286,102 @@ function info() {
   }
 }
 function stats() {
-  generateOverlayFrameText("This should be the stats overlay.")
+  let title = ""
+  if(this.page_data["play_mode"] == "DAILY"){
+    title = "Daily Puzzle Stats"
+  }
+  else if(this.page_data["play_mode"] == "PRACTICE"){
+    title = "Practice Puzzle Stats"
+  }
+  
+  let master_div = document.createElement("div")
+  master_div.classList.add("stats_master")
+  let title_div = document.createElement("div")
+  title_div.classList.add("stats_title")
+  title_div.innerHTML = "<h2>" + title + "</h2>"
+  master_div.appendChild(title_div)  
+  let bar_chart = document.createElement("div")
+  master_div.appendChild(bar_chart)
+  let streak_count = document.createElement("div")
+  let stats_source = ""
+  if(this.page_data["play_mode"] == "DAILY"){
+    stats_source = this.page_data["daily_puzzle_stats"]
+
+    streak_count.innerHTML = "Streak: " + stats_source["streak"]
+    bar_chart.appendChild(streak_count)
+  }
+  else{
+    stats_source = this.page_data["practice_puzzle_stats"]
+  }
+  let guesses_left_4 = document.createElement("div")
+  guesses_left_4.innerHTML = "<b>4 guesses left:</b> " + stats_source["guesses_4"] + " games"
+  let guesses_left_3 = document.createElement("div")
+  guesses_left_3.innerHTML = "<b>3 guesses left:</b> " + stats_source["guesses_3"] + " games"
+  let guesses_left_2 = document.createElement("div")
+  guesses_left_2.innerHTML = "<b>2 guesses left:</b> " + stats_source["guesses_2"] + " games"
+  let guesses_left_1 = document.createElement("div")
+  guesses_left_1.innerHTML = "<b>1 guess left:</b> " + stats_source["guesses_1"] + " games"
+  bar_chart.appendChild(guesses_left_4)
+  bar_chart.appendChild(guesses_left_3)
+  bar_chart.appendChild(guesses_left_2)
+  bar_chart.appendChild(guesses_left_1)
+  if(this.page_data["play_mode"] == "DAILY"){
+    let last_solved = document.createElement("div")
+    last_solved.innerHTML = "Last solved: " + stats_source["last_solved"]
+    bar_chart.appendChild(last_solved)
+  }
+  generateOverlayFrameElement(master_div)
 }
-function getPuzzle(number) {
-  return $.getJSON("puzzles/" + number + "/wotd.json", function (data) {
-    return data;
-  })
+function settings(){
+  title = "Settings"
+  master_div = document.createElement("div")
+  master_div.classList.add("settings_master")
+  title_div = document.createElement("div")
+  title_div.classList.add("settings_title")
+  title_div.innerHTML = "<h2>" + title + "</h2>"
+  master_div.appendChild(title_div)  
+  toggle_bar = document.createElement("div")
+  daily_puzzle = document.createElement("div")
+  daily_puzzle.innerHTML = "Daily Puzzle"
+  daily_puzzle.id = "daily_puzzle_div"
+  practice = document.createElement("div")
+  practice.innerHTML = "Practice"
+  practice.id = "practice_div"
+  daily_puzzle.classList.add("puzzle_option")
+  practice.classList.add("puzzle_option")
+  daily_puzzle.addEventListener("click", function () { updatePuzzleSelection("DAILY") })
+  practice.addEventListener("click", function(){updatePuzzleSelection("PRACTICE")})
+  if(this.page_data["play_mode"] == "DAILY"){
+    daily_puzzle.classList.add("puzzle_option_selected")
+    practice.classList.add("puzzle_option_unselected")
+
+  }
+  else{
+    daily_puzzle.classList.add("puzzle_option_unselected")
+    practice.classList.add("puzzle_option_selected")
+
+  }
+
+  toggle_bar.appendChild(daily_puzzle)
+  toggle_bar.appendChild(practice)
+  master_div.appendChild(toggle_bar)
+  generateOverlayFrameElement(master_div)
+}
+function getPuzzle() {
+  if(this.page_data["play_mode"] == "DAILY"){
+    return $.getJSON("puzzles/daily/" + this.date + "/puzzle.json", function (data) {
+      return data;
+    })
+  }else{
+    return $.getJSON("puzzles/practice/" + this.page_data["practice_puzzle_state"]["current_puzzle_number"] + "/puzzle.json", function (data) {
+      return data;
+    })
+  }
+
 
 }
 function generateOverlayFrameText(innerHTML) {
+  destroyOverlayFrame()
   var new_overlayframe = document.createElement("div")
   var notification_text = document.createElement('div')
   notification_text.style.maxWidth = "750px"
@@ -168,6 +400,7 @@ function generateOverlayFrameText(innerHTML) {
   return new_overlayframe
 }
 function generateOverlayFrameElement(element) {
+  destroyOverlayFrame()
   var new_overlayframe = document.createElement("div")
 
   new_overlayframe.id = "overlay"
@@ -181,7 +414,10 @@ function generateOverlayFrameElement(element) {
   return new_overlayframe
 }
 function destroyOverlayFrame() {
-  document.getElementById("overlay").remove();
+  let overlay = document.getElementById("overlay")
+  if(overlay != null ){
+    overlay.remove()
+  }
 }
 function targetSelect(number) {
   if (number == 0) {
@@ -244,7 +480,13 @@ function setCookie(cname, cvalue, exdays) {
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
+function plusExplanationVerse(n){
+ showExplanation(this.explanation_slideIndex +=n)
+}
+function currentExplanation(n){
+  this.explanation_slideIndex = n
+  showExplanation(n)
+}
 // Thumbnail image controls
 function currentSlide(n) {
   slideIndex = n;
@@ -270,19 +512,27 @@ function showSlides(n) {
   dots[slideIndex - 1].classList.add("active")
   dots[slideIndex - 1].style.backgroundColor = number_to_light_color(slideIndex - 1);
 
-  /*  if(this.game_state["solved"][n]){
-      word_indicator.classList.add("word_appear")
-      
-    }
-    else{
-      word_indicator.classList.remove("word_appear")
-  
-    }
-    if(this.game_state["solved"][0]){
-  
-    }*/
   updateButtonsOnGameState()
+}
+function showExplanation(n) {
+  let i;
+  let slides = document.getElementsByClassName("explanationSlide");
+  if (n > slides.length) { this.explanation_slideIndex = 1 }
+  if (n < 1) { this.explanation_slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[this.explanation_slideIndex - 1].style.display = "block";
 
+  let dots = document.getElementsByClassName("explanation_dot");
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove("active");
+    dots[i].classList.add("inactive");
+    dots[i].style.backgroundColor = number_to_dark_color(i);
+  }
+  dots[this.explanation_slideIndex - 1].classList.remove("inactive")
+  dots[this.explanation_slideIndex - 1].classList.add("active")
+  dots[this.explanation_slideIndex - 1].style.backgroundColor = number_to_light_color(this.explanation_slideIndex - 1);
 }
 function define(number) {
   word_to_define = this.game_state["button_to_word_index"][number]
@@ -548,9 +798,11 @@ function check() {
     }
   }
   if (found_all) {
+    recordGameWin(this.game_state["guesses_left"])
     info()
   }
   if(this.game_state["guesses_left"] == 0){
+    recordGameLoss()
     info()
   }
   /*win*/
@@ -624,6 +876,7 @@ function constructExplanation(title) {
   slideshow_div.classList.add("explanation_slideshow")
   master_div.appendChild(slideshow_div)
   nav_div = document.createElement("div")
+  nav_div.classList.add("nav_div")
   verses = []
   for (i = 0; i < 4; i++) {
     verses.push(document.createElement("div"))
@@ -634,6 +887,7 @@ function constructExplanation(title) {
     ref_div.innerHTML = "<h2>" + reference + "</h2>"
     verses[i].appendChild(ref_div)
     data = document.createElement("div")
+    verses[i].classList.add("explanationSlide")
     for(j = 0; j < verse_data["verse_data"].length; j++){
       data_item = document.createElement("div")
       data_item.classList.add("explanation_verse_data_item")
@@ -643,24 +897,65 @@ function constructExplanation(title) {
       if(verse_data["verse_data"][j]["Str Grk"] == this.puzzle["wotd"]["number"]){
         data_item.style.backgroundColor = number_to_light_color(5)
       }
-      else{
+      else if(verse_data["verse_data"][j]["Str Grk"] == this.puzzle["other_words"][0]["number"]){
+        data_item.style.backgroundColor = number_to_light_color(0)
+      }
+      else if(verse_data["verse_data"][j]["Str Grk"] == this.puzzle["other_words"][1]["number"]){
+        data_item.style.backgroundColor = number_to_light_color(1)
+      }
+      else if(verse_data["verse_data"][j]["Str Grk"] == this.puzzle["other_words"][2]["number"]){
+        data_item.style.backgroundColor = number_to_light_color(2)
+      }else if(verse_data["verse_data"][j]["Str Grk"] == this.puzzle["other_words"][3]["number"]){
+        data_item.style.backgroundColor = number_to_light_color(3)
+      }
+        else
+        {
         data_item.style.backgroundColor = "#FFFFFF"
       }
       data.appendChild(data_item)
     }
     verses[i].appendChild(data)
     verse_button = document.createElement("div")
-    verse_button.classList.add("dot")
+    verse_button.classList.add("explanation_dot")
+    if(i == 0){
+      verse_button.addEventListener("click", function () { currentExplanation(1) })
+    }
+    else if(i == 1){
+      verse_button.addEventListener("click", function () { currentExplanation(2) })
+    }
+    else if(i == 2){
+      verse_button.addEventListener("click", function () { currentExplanation(3) })
+    }
+    else if(i == 3){
+      verse_button.addEventListener("click", function () { currentExplanation(4) })
+    }
     verse_button.innerHTML = reference
     nav_div.appendChild(verse_button)
   }
+
+  let backward_button = document.createElement("a")
+  backward_button.innerHTML = "&#10094;"
+  backward_button.classList.add("prev_explanation")
+  backward_button.addEventListener("click", function () { plusExplanationVerse(-1) })
+
+  let forward_button = document.createElement("a")
+  forward_button.innerHTML = "&#10095;"
+  forward_button.classList.add("next_explanation")
+  forward_button.addEventListener("click", function () { plusExplanationVerse(1) })
+  slideshow_div.appendChild(forward_button)
+
+  slideshow_div.appendChild(backward_button)
   master_div.appendChild(nav_div)
   next_button = document.createElement("div")
-  next_button.innerHTML = "Next"
+  next_button.innerHTML = "Stats"
+  next_button.classList.add("puzzle_option")
+  next_button.style.backgroundColor = "#BBBBBB"
+  next_button.addEventListener("click", function () { stats() })
   master_div.appendChild(next_button)
   return master_div
 }
 function generateExplanationOverlay(winning) {
+  
   if (winning) {
     header_text = "Congratulations! You finished with " + this.game_state["guesses_left"] + " guesses left!"
   } else {
@@ -668,4 +963,32 @@ function generateExplanationOverlay(winning) {
   }
   explanation_div = constructExplanation(header_text)
   generateOverlayFrameElement(explanation_div)
+  currentExplanation(1)
+}
+function updatePuzzleSelection(puzzle_type){
+  if(puzzle_type == "DAILY" && this.page_data["play_mode"] != "DAILY"){
+      this.page_data["play_mode"] = "DAILY"
+      daily_puzzle_div = document.getElementById("daily_puzzle_div")
+      daily_puzzle_div.classList.add("puzzle_option_selected")
+      daily_puzzle_div.classList.remove("puzzle_option_unselected")
+
+      practice_div = document.getElementById("practice_div")
+      practice_div.classList.add("puzzle_option_unselected")
+      practice_div.classList.remove("puzzle_option_selected")
+
+      load_puzzle()
+  }
+  else if(puzzle_type == "PRACTICE" && this.page_data["play_mode"] != "PRACTICE"){
+    this.page_data["play_mode"] = "PRACTICE"
+
+    daily_puzzle_div = document.getElementById("daily_puzzle_div")
+    daily_puzzle_div.classList.add("puzzle_option_unselected")
+    daily_puzzle_div.classList.remove("puzzle_option_selected")
+
+    practice_div = document.getElementById("practice_div")
+    practice_div.classList.add("puzzle_option_selected")
+    practice_div.classList.remove("puzzle_option_unselected")
+    load_puzzle()
+  }
+  update_local_storage()
 }
